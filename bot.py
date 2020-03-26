3c3699a6ad0485bbe14852823f2f39fd8b8b0fd3
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2020-03-26 21:23:41 +0100

    Vastly improve bot

--- bot.py
+++ bot.py
@@ -19,6 +19,14 @@ load_dotenv()
 bot = commands.Bot("!", case_insensitive=True)
 
 
+def plural(count, name):
+    s = f"{count} {name}"
+    if count != 1:
+        s += "s"
+
+    return s
+
+
 class Academy(commands.Cog):
     TIMEOUT = aiohttp.ClientTimeout(total=5)
 
@@ -34,8 +42,8 @@ class Academy(commands.Cog):
                     self.get_game_data(self.current_game_id)
                 )
 
-                print(f"Got game id from saved state: {self.current_game_id}")
-            except NoResultFound:
+                print(f"Got game id from saved state: {self.current_game_id}.")
+            except (NoResultFound, ClientResponseError):
                 self.current_game_id = None
                 print("No game id saved state.")
 
@@ -70,31 +78,39 @@ class Academy(commands.Cog):
                 discord_id = link.discord_id
                 return self.bot.get_user(discord_id).mention
             except NoResultFound:
-                return f"unknown (please user !link) player"
+                return player_stats["username"]
 
+    async def send_info(self, game_data):
+        player_stats = game_data["player_stats"]
+        player_count = len(player_stats)
+        card_count = len(game_data["cards"])
+        player_index = card_count % player_count
 
-    async def send_info(self, player_stats):
-        player_name = await self.get_player_name(player_stats)
-        await self.channel.send(
-            f"""Now it's {player_name}'s turn:
-To be on level they have to have drunk {player_stats['full_beers']} beer(s) and {player_stats['extra_sips']} sip(s) to be on level."""
+        previous_player_name = self.get_player_name(
+            player_stats[(player_index - 1) % len(player_stats)]
         )
+        current_player_stats = player_stats[player_index]
+        player_name = self.get_player_name(current_player_stats)
 
+        message = ""
+        if game_data["cards"]:
+            card = game_data["cards"][-1]
+            message += f"{previous_player_name} just got a {card['value']}.\n\n"
+
+        message += f"""Now it's {player_name}'s turn:
+To be on level they have to have drunk {plural(current_player_stats['full_beers'], 'beer')} and {plural(current_player_stats['extra_sips'], 'sip')}."""
+
+        await self.channel.send(message)
 
     async def post_game_update(self, game_data):
         player_count = len(game_data["player_stats"])
-        card_count = len(game_data["cards"])
-        player_index = card_count % player_count
-        player_count = len(game_data["player_stats"])
         total_card_count = player_count * 13
-        if card_count == total_card_count:
+        if len(game_data["cards"]) == total_card_count:
             await self.channel.send(
                 f"The game has finished, look at the stats at https://academy.beer/games/{self.current_game_id}/"
             )
         else:
-            card_count = len(game_data["cards"])
-            player_index = card_count % player_count
-            await self.send_info(game_data, game_data["player_stats"][player_index])
+            await self.send_info(game_data)
 
     async def get_game_data(self, game_id):
         async with aiohttp.ClientSession(
@@ -103,7 +119,8 @@ To be on level they have to have drunk {player_stats['full_beers']} beer(s) and 
             async with session.get(
                 f"https://academy.beer/api/games/{game_id}/"
             ) as response:
-                return await response.json()
+                res = await response.json()
+                return res
 
     async def get_username(self, user_id):
         async with aiohttp.ClientSession(
@@ -116,71 +133,85 @@ To be on level they have to have drunk {player_stats['full_beers']} beer(s) and 
 
     async def background_task(self):
         while True:
-            async with self.lock:
-                if self.current_game_id:
+            if self.current_game_id:
+                try:
                     new_game_data = await self.get_game_data(self.current_game_id)
-                    if len(new_game_data["cards"]) != len(self.game_data["cards"]):
-                        await self.post_game_update(new_game_data)
+                except ClientResponseError:
+                    await self.channel.send(
+                        "Failed to get game data, unfollowing game."
+                    )
+                    await self.set_followed_game(None)
 
-                    self.game_data = new_game_data
+                if len(new_game_data["cards"]) != len(self.game_data["cards"]):
+                    await self.post_game_update(new_game_data)
+
+                self.game_data = new_game_data
 
             await asyncio.sleep(1)
 
-    @commands.command(name="follow")
-    async def follow(self, ctx, *args):
-        async with self.lock:
-            try:
-                game_id = int(args[0])
-            except:
-                await ctx.send("Invalid game id!")
-                return
-
-            try:
-                self.game_data = await self.get_game_data(game_id)
-            except ClientResponseError:
-                await ctx.send("Couldn't get game data! Does the game exist?")
-                return
-
-            self.current_game_id = game_id
-
-            with session_scope() as session:
-                session.query(State).delete()
+    async def set_followed_game(self, game_id):
+        self.current_game_id = game_id
+        with session_scope() as session:
+            session.query(State).delete()
+            if self.current_game_id:
                 session.add(State(followed_game=self.current_game_id))
 
-            await self.update_status()
-            await ctx.send(f"Now following game {self.current_game_id}.")
-            await self.post_game_update(self.game_data)
+        await self.update_status()
+
+    @commands.command(name="follow")
+    async def follow(self, ctx, *args):
+        try:
+            game_id = int(args[0])
+        except:
+            await ctx.send("Invalid game id!")
+            return
+
+        try:
+            self.game_data = await self.get_game_data(game_id)
+        except ClientResponseError:
+            await ctx.send("Couldn't get game data! Does the game exist?")
+            return
+
+        await self.set_followed_game(game_id)
+        await ctx.send(f"Now following game {self.current_game_id}.")
+        await self.post_game_update(self.game_data)
+
+    @commands.command(name="unfollow")
+    async def unfollow(self, ctx, *args):
+        game_id = self.current_game_id
+        if game_id:
+            await self.set_followed_game(None)
+            await ctx.send(f"Unfollowed game {game_id}.")
+        else:
+            await ctx.send(f"Not currently following any game!")
 
     @commands.command(name="link")
     async def link(self, ctx, *args):
-        async with self.lock:
+        try:
+            academy_id = int(args[0])
+        except:
+            await ctx.send("Invalid academy id!")
+            return
+
+        try:
+            username = await self.get_username(academy_id)
+        except:
+            await ctx.send("Couldn't get user data! Does the user exist?")
+            return
+
+        discord_id = ctx.author.id
+
+        with session_scope() as session:
+            session.query(Link).filter(Link.discord_id == discord_id).delete()
             try:
-                academy_id = int(args[0])
-            except:
-                await ctx.send("Invalid academy username id!")
+                session.add(Link(discord_id=discord_id, academy_id=academy_id))
+            except IntegrityError:
+                await ctx.send(
+                    f"{ctx.author.mention} that academy user is already linked to someone else!"
+                )
                 return
 
-            try:
-                username = await self.get_username(academy_id)
-            except:
-                await ctx.send("Couldn't get user data! Does the user exist?")
-                return
-
-            discord_id = ctx.author.id
-
-            with session_scope() as session:
-                session.query(Link).filter(Link.discord_id == discord_id).delete()
-                try:
-                    session.add(Link(discord_id=discord_id, academy_id=academy_id))
-                except IntegrityError:
-                    await ctx.send(
-                        f"{ctx.author.mention} that academy user is already linked to someone else!"
-                    )
-                    return
-
-            await ctx.send(
-                f"{ctx.author.mention} is now linked with {username} on academy"
-            )
+        await ctx.send(f"{ctx.author.mention} is now linked with {username} on academy.")
 
     @commands.command(name="test")
     async def test(self, ctx, *args):
