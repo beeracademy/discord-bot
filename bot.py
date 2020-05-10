beeracademy/discord-bot

commit 71401598205a7c66376cc28fc1834ac97334c4c7

Add !restart command

bot.py
@@ -3,6 +3,7 @@ import io
 import logging
 import os
 import random
+import sys
 from functools import wraps
 from typing import Optional
 
@@ -47,8 +48,6 @@ AU_PASSWORD = os.environ["AU_PASSWORD"]
 
 MAX_FINISHED_GAMES = 10
 MAX_DISCORD_MESSAGE_LENGTH = 2000
-
-bot = commands.Bot("!", case_insensitive=True)
 
 
 def run_with_timeout(f, fargs=[], fkwargs={}, *args, **kwargs):
@@ -644,13 +643,32 @@ class Academy(commands.Cog):
         join_url = await zoom.generate_join_url(AU_ID, AU_PASSWORD)
         await ctx.send(f"Generated new zoom meeting: {join_url}")
 
+    @commands.command(name="restart")
+    @commands.is_owner()
+    async def restart(self, ctx):
+        await ctx.send("Restarting...")
+        await self.bot.change_presence(activity=Game(name="Restarting..."))
+        await self.bot.close()
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.id == FURA_ID:
             await self.fura(message.channel, text=message.content)
 
 
-bot.add_cog(Academy(bot))
+def init_bot():
+    bot = commands.Bot("!", case_insensitive=True)
+    bot.add_cog(Academy(bot))
+    return bot
+
+
+async def main():
+    bot = init_bot()
+    await bot.start(DISCORD_TOKEN)
+
+    print("Restarting...")
+    os.execvp("python", ["python", *sys.argv])
+
 
 if __name__ == "__main__":
-    bot.run(DISCORD_TOKEN)
+    asyncio.run(main())

commit 708982d94ca2dd31b204620f0c417799287ad59e

Fix background tasks
This bug was introduced by 71401598205a7c66376cc28fc1834ac97334c4c7

bot.py
@@ -195,6 +195,7 @@ class Academy(commands.Cog):
         self.game_datas = {}
         self.update_game_datas.start()
         self.first_on_ready = True
+        self.should_restart = False
 
     def cog_unload(self):
         self.update_game_datas.cancel()
@@ -646,6 +647,7 @@ class Academy(commands.Cog):
     @commands.command(name="restart")
     @commands.is_owner()
     async def restart(self, ctx):
+        self.should_restart = True
         await ctx.send("Restarting...")
         await self.bot.change_presence(activity=Game(name="Restarting..."))
         await self.bot.close()
@@ -658,17 +660,19 @@ class Academy(commands.Cog):
 
 def init_bot():
     bot = commands.Bot("!", case_insensitive=True)
-    bot.add_cog(Academy(bot))
-    return bot
+    academy = Academy(bot)
+    bot.add_cog(academy)
+    return academy
 
 
-async def main():
-    bot = init_bot()
-    await bot.start(DISCORD_TOKEN)
+def main():
+    academy = init_bot()
+    academy.bot.run(DISCORD_TOKEN)
 
-    print("Restarting...")
-    os.execvp("python", ["python", *sys.argv])
+    if academy.should_restart:
+        print("Restarting...")
+        os.execvp("python", ["python", *sys.argv])
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()